5ddce254af049f7d273017c4a418e9d09fc21069
Stylishking1706 <85835217+Stylishking1706@users.noreply.github.com>
2021-06-13 23:19:07 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -150,7 +150,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Stylishking1706- May I Coming")
+        update.effective_message.reply_text("Stylishking1706- Love is My life")
 
 
 def send_start(bot, update):
